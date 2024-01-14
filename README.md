a5ee42169076bdf7e3aeef61a4565c0e00210138
your-SamGreenwood <your-sagg_84@hotmail.com>
2024-01-14 18:04:52 -0400

    added references to readme.md, added to js and changed js link in html

--- README.md
+++ README.md
@@ -10,17 +10,30 @@ SO THAT I can manage my time effectively
 Acceptance Criteria
 
 GIVEN I am using a daily planner to create a schedule
+
 WHEN I open the planner
+
 THEN the current day is displayed at the top of the calendar
+
 WHEN I scroll down
+
 THEN I am presented with time blocks for standard business hours of 9am to 5pm
+
 WHEN I view the time blocks for that day
-THEN each time block is color-coded to indicate whether it is in the past, present, or future
+
+THEN each time block is color-coded to indicate whether it is in the past,
+ present, or future
+
 WHEN I click into a time block
+
 THEN I can enter an event
+
 WHEN I click the save button for that time block
+
 THEN the text for that event is saved in local storage
+
 WHEN I refresh the page
+
 THEN the saved events persist
 
 Create a simple calendar application that allows a user to save events for each hour of a typical working day (9am–5pm). This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
@@ -33,9 +46,12 @@ The starter code uses the Day.jsLinks to an external site. library to work with 
 
 const dayjs = require('dayjs')
 
-![js script](image.png)
-
 **ask-296418 Learning Assistant Help:**
+
 
 ![Alt text](assets/images/this-dayjs.png)
 ![Alt text](<assets/images/learning asst-reference.png>)
+
+**Asked for help from Classmate Shawn Meister**
+
+![Alt text](image.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
-//displays current date in the header
-$(function () {
-  var now = dayjs();
+$(document).ready(function () {
+  // Function to update the current day in the header
+  function updateCurrentDay() {
+    var now = dayjs();
+    $("#currentDay").text(now.format("MMMM D, YYYY"));
+  }
 
-  $("#currentDay").text(dayjs().format("MMMM D, YYYY"));
+  // Display the current day when the page loads
+  updateCurrentDay();
 
-  //saves user input to local storage when the save button is clicked
+  // Save user input to local storage when the save button is clicked
   $(".saveBtn").on("click", function () {
     var timeBlockId = $(this).parent().attr("id");
     var userInput = $(this).siblings(".description").val();
@@ -13,22 +17,34 @@ $(function () {
     console.log("userInput");
   });
 
-  //gets the current hour to then compare to each timeblock
-  var currentHour = now.hour();
+  // Compare current time with each time block and apply appropriate classes
+  function updateHourlyBlocks() {
+    var currentHour = dayjs().hour();
 
-  $(".time-block").each(function () {
-    var blockHour = parseInt($(this).attr("id").split("-")[1]);
+    $(".time-block").each(function () {
+      var $timeBlock = $(this);
+      var blockHour = parseInt($timeBlock.attr("id").split("-")[1]);
 
-    // Remove all classes and add the appropriate class based on the comparison
-    $(this).removeClass("past present future").addClass(
-      blockHour < currentHour
-        ? "past"
-        : blockHour === currentHour
-        ? "present"
-        : "future"
-    );
-  });
+  // Remove all classes and add the appropriate class based on the comparison
+      if (blockHour < currentHour) {
+        $timeBlock.removeClass("present future").addClass("past");
+      } else if (blockHour === currentHour) {
+        $timeBlock.removeClass("past future").addClass("present");
+      } else {
+        $timeBlock.removeClass("past present").addClass("future");
+      }
+    });
+  }
 
+  // Apply the color-coding initially
+  updateHourlyBlocks();
+
+  // Periodically update the color-coding every minute
+  setInterval(function () {
+    updateHourlyBlocks();
+  }, 60000);
+
+  // Load saved events from local storage when the page loads
   $(".time-block").each(function () {
     var timeBlockId = $(this).attr("id");
     var savedInput = localStorage.getItem(timeBlockId);
